6d3f5c759789402e1b6a8716d6cd9fa3ff9cf1e2
nexxoxd <cri@pafcat.net>
2024-06-12 21:03:23 -0400

    Update caluladora.py

--- caluladora.py
+++ caluladora.py
@@ -1,11 +1,8 @@
-open =int(input("precione del 1, 2 , 3, 4,"))
+K = int(input("numero"))
+L = int(input("numero"))
 
-cal = int(input("numero"))
-cu = int(input("numero"))
-if open == 1 :
-    suma = cal + cu
-    print(suma) 
-elif open == 2:
-    resta = cal - cu
-print(resta)
+KL = K * L
+
+
+print(KL)
 
